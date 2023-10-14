72a34ae82ac60813ccd37f560667c843a6a77006
Vladislav <45711797+Dragonwlad@users.noreply.github.com>
2023-10-14 12:59:01 +0300

    add env exampleÑ

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.getenv('SECRET_KEY') 
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', default='False') == 'True'
